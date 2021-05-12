e45976170b70d1680c5854411df87590d3f8b8d3
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-12 13:29:12 +0200

    Fix issues last commit, Adds db connection with mongoin db.js and connection with express in api.js

--- api/db.js
+++ api/db.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const router = require('./router');
-const app = express();
-const cors = require('cors');
-const db = require('./db');
-const port = 3000;
+const mongoose = require('mongoose')
 
-app.use(express.json());
-app.use(cors());
+const url = process.env.MONGO_SERVER || 'mongodb://localhost:27017/wmo'
+const db = mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(db => console.log('DB is conencted to', 
+    db.connection.host)).then( console.log(process.env.MONGO_SERVER))
+.catch(error => console.error(error));
 
-app.use(router);
-db.then(()=>{
-    app.listen(port, () => {
-        console.log('I am ready!!');
-    })
 
-}).catch(console.log);
+module.exports = db
