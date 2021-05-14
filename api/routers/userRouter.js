bf6b1d6d69c2d11a48f8ead29480266070764d6e
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-05-14 12:36:20 +0200

    Fix UserRouter

--- api/routers/userRouter.js
+++ api/routers/userRouter.js
@@ -4,100 +4,110 @@ const User = require('../models/userModel');
 
 // Create User
 
-router.post('/', async (req, res)  => {
-
-    try {
-        const newUser = new User(req.body);
-        const result = await userController.create(newUser);
-        res.json(result);
-    } catch (error) {
-        console.log(error)
+router.post ('/', async (req, res) => {
+    try{
+        let user = await userController.createUser(req.body);
+        let status = 'User created !';
+        return res.json({user, status});
+    }catch(error){
+        return res.status(500).json({
+            message: error.message
+        });
     }
 });
 
+
 // Login User
 
-router.post('/login', async (req, res) => {
-
-    try {
-        const {email, password} = req.body;
-        const jwt = await userController.login(email, password);
-        const token = jwt.token;
-        const user = jwt.user;
-        res.json(token, user);
-    } catch (error) {
-        return res.status(401).json({
+router.post ('/login', async (req, res) => {
+    try{
+        let email = req.body.email;
+        let password = req.body.password;
+        let jwt = await userController.login(email, password);
+        let token = jwt.token;
+        let user = jwt.user;
+        let status = 'Success Login !';
+        res.json({token, user, status});
+    }catch(error){
+        return res.status(500).json({
             message: error.message
-        })
+        });
     }
 });
 
 // Search User by Id
 
-// const adminSearchByIdHandler = async (req, res) => {
-
-//     try {
-//         const id = req.params.id;
-//         const result = await userController.searchById(id);
-//         res.json({result);
-//     } catch (error) {
-//         console.log(error)
-//     }
-// };
+router.get ('/:id', async (req, res) => {
+    try{
+        let id = req.params.id;
+        let userId = await userController.userById(id);
+        return res.json(userId);
+    }catch(error){
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
 
 // // Search All Users
 
-// const adminIndexAllUserHandler = async (req, res) => {
-
-//     try {
-//         const result = await userController.userAll();
-//         console.log(result)
-//         res.json({result, date: new Date});
-//     } catch (error) {
-//         console.log(error)
-//     }
-// };
+router.get ('/', async (req, res) => {
+    try{
+        let allUsers = await userController.userAll();
+        return res.json(allUsers);
+    }catch(error){
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
 
 
 // Update User
 
-router.put('/:id', async (req,res) => {
-
-    try {
-        const updateUser = req.body;
-        const id = req.params.id;
-        const result = await userController.update(id, updateUser);
-        res.json(result);
-    } catch (error) {
-        console.log(error)
+router.put ('/:id', async (req, res) => {
+    try{
+        let id = req.params.id;
+        let user = req.body;
+        let userUpdate = await userController.updateUser(id, user);
+        let status = 'User updated successfully !';
+        res.json({status, userUpdate});
+    }catch (error){
+        return res.status(500).json({
+            message: error.message
+        });
     }
 });
 
 // // Delete User
 
-// const deleteHandler = async (req, res) => {
-
-//     try {
-//         const id = req.params.id;
-//         const result = await userController.delete(id);
-//         res.json({result);
-//     } catch (error) {
-//         console.log(error)
-//     }
-// };
+router.delete ('/:id', async (req, res) => {
+    try{
+        let id = req.params.id;
+        let userDelete = await userController.deleteUser(id);
+        let status = 'User deleted successfully !';
+        res.json({status, userDelete});
+    }catch (error){
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
 
 // // Delete All
 
-// const deleteAllUserHandler = async (req, res) => {
+router.delete ('/', async (req, res)  => {
 
-//     try {
-//         const result = await userController.deleteMany();
-//         res.json({result);
-//     } catch (error) {
-//         console.log(error)
-//     }
-// };
-
+    try{
+        let userDelete = await userController.deleteUser();
+        let status = 'User deleted successfully !';
+        res.json({status, userDelete});
+        }catch (error){
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
 
 
 module.exports = router;
